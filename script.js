8ab669d0d2782eda112751726795b3f7d4f41539
Sadman-123 <19201103123@cse.bubt.edu.bd>
2024-09-17 20:08:33 +0600

    changes

--- script.js
+++ script.js
@@ -1,28 +1,35 @@
-const app=require('express')();
-const cors=require('cors');
-const bdy=require('body-parser');
-const DB=require('./db/bankdb');
+const app = require('express')();
+const cors = require('cors');
+const bdy = require('body-parser');
+const DB = require('./db/bankdb');
+
 app.use(cors());
-app.use(bdy.urlencoded({extended:true}));
+app.use(bdy.urlencoded({ extended: true }));
 app.use(bdy.json());
-app.post('/insert',async (req,res)=>{
-    const lol=new DB({
-        time:req.body.time,
-        add:req.body.add,
-        money_transfer:req.body.money_transfer,
-        name:req.body.name
-    });
-    await lol.save()
-    .then(()=>res.status(200).send('Inserted'))
-    .catch(e=>console.log(e))
-})
-app.get('/api/:nm',async (req,res)=>{
-    const lox=await DB.find({
-        name:req.params.nm
-    })
-    .then((dat)=>{
-        res.status(200).json(dat)
-    })
-    .catch(e=>console.log(e))
-})
-app.listen(3000,()=>console.log('Listening'))
+
+app.post('/insert', async (req, res) => {
+  const lol = new DB({
+    time: req.body.time,
+    add: req.body.add,
+    money_transfer: req.body.money_transfer,
+    name: req.body.name,
+  });
+  try {
+    await lol.save();
+    res.status(200).send('Inserted');
+  } catch (e) {
+    console.log(e);
+    res.status(500).send('Error');
+  }
+});
+
+app.get('/api/:nm', async (req, res) => {
+  try {
+    const data = await DB.find({ name: req.params.nm });
+    res.status(200).json(data);
+  } catch (e) {
+    console.log(e);
+    res.status(500).send('Error');
+  }
+});
+module.exports = app;
