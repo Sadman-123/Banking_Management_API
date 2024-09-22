3fc5c29a1a8ffe8da24fabdfbd4b4a268032a86c
Sadman-123 <19201103123@cse.bubt.edu.bd>
2024-09-22 13:11:22 +0600

    rearranging

--- script.js
+++ script.js
@@ -9,6 +9,49 @@ const jwt=require('jsonwebtoken');
 app.use(cors());
 app.use(bdy.urlencoded({ extended: true }));
 app.use(bdy.json());
+app.post('/register',async (req,res)=>{
+  const {username,password}=req.body;
+  try{
+    const user=await userDB.findOne({username:username});
+    if(user)
+    {
+      return res.status(400).send("User Already Exists")
+    }
+    bcrypt.hash(password, 10, async function(err, hash) {
+      const newuser=new userDB({
+        username:username,
+        password:hash,
+        time:new Date().toUTCString()
+      })
+      await newuser.save()
+      res.status(200).send("User Created")
+  });
+  }
+  catch{
+    res.status(400).send('Something went wrong')
+  }
+});
+app.post('/login',async (req,res)=>{
+  const {username,password}=req.body;
+  const user=await userDB.findOne({username:username});
+  if(user)
+  {
+    bcrypt.compare(password, user.password, function(err, result) {
+     if(result){
+      const token=jwt.sign({username:username,id:user.id},process.env.secret,{expiresIn:'1h'});
+      res.status(200).json({
+        token:token
+      })
+     }
+     else{
+      res.status(400).send('Password not matched')
+     }
+  });
+  }
+  else{
+    res.status(404).send("User not Exists")
+  }
+})
 app.post('/insert', async (req, res) => {
   const lol = new DB({
     time: req.body.time,
@@ -57,47 +100,3 @@ app.get('/api/sum/:nm', async (req, res) => {
 });
 app.listen(process.env.port,()=>{console.log(`Listening ${process.env.port}`)})
 
-app.post('/register',async (req,res)=>{
-  const {username,password}=req.body;
-  try{
-    const user=await userDB.findOne({username:username});
-    if(user)
-    {
-      return res.status(400).send("User Already Exists")
-    }
-    bcrypt.hash(password, 10, async function(err, hash) {
-      const newuser=new userDB({
-        username:username,
-        password:hash,
-        time:new Date().toUTCString()
-      })
-      await newuser.save()
-      res.status(200).send("User Created")
-  });
-  }
-  catch{
-    res.status(400).send('Something went wrong')
-  }
-
-});
-app.post('/login',async (req,res)=>{
-  const {username,password}=req.body;
-  const user=await userDB.findOne({username:username});
-  if(user)
-  {
-    bcrypt.compare(password, user.password, function(err, result) {
-     if(result){
-      const token=jwt.sign({username:username,id:user.id},process.env.secret,{expiresIn:'1h'});
-      res.status(200).json({
-        token:token
-      })
-     }
-     else{
-      res.status(400).send('Password not matched')
-     }
-  });
-  }
-  else{
-    res.status(404).send("User not Exists")
-  }
-})
